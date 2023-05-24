f27193bc66070f967d64ac7cb8d2725352de25a1
gokulapriyandckap <bgokulapriyandckap@gmail.com>
2023-05-24 11:25:32 +0530

    problem solved

---
+++ reverse_number.php
@@ -0,0 +1,13 @@
+<?php
+    $number =readline("Enter any Number:");
+    if (is_numeric($number)) {
+            echo strrev(floor($number));
+    
+    }
+    else
+    {
+        echo "It Is Not A Number";
+    }
+
+
+    
