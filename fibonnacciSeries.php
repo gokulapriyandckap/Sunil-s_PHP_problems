ce7fcd7336956e279fc3ffa9c8d1be4d01bbfcfb
gokulapriyandckap <bgokulapriyandckap@gmail.com>
2023-05-25 15:40:03 +0530

    problem solved

--- fibonnacciSeries.php
+++ fibonnacciSeries.php
@@ -1,12 +1,17 @@
 <?php
+
+ function fibannacciSeries($n)
+{
 $i = 0;
 $n1 = 0;
 $n2= 1;
 echo $n1."\n".$n2."\n";
-while ($i <= 10) {
+while ($i <= $n) {
     $n3 = $n1 + $n2;
     $n1 = $n2;
     $n2 = $n3;
     echo $n3."\n";
     $i += 1;
 }
+}
+fibannacciSeries (10);
